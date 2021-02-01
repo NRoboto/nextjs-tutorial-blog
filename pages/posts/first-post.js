276b26b8bf27b991f0e8c54d70477642202bf90e
Mike <mnightingale@tutanota.com>
2021-02-01 14:01:25 +0000

    Add first post title

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,11 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const firstPost = () => (
   <>
+    <Head>
+      <title>First Post</title>
+    </Head>
     <h1>First Post!</h1>
     <h2>
       <Link href="/">
