b87945c5d2120d026c58d4192f472702fd11377b
Mike <mnightingale@tutanota.com>
2021-02-01 19:23:21 +0000

    Add dynamic route for posts

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+const Post = ({ postData }) => (
+  <Layout>
+    {postData.title}
+    <br />
+    {postData.id}
+    <br />
+    {postData.date}
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+  </Layout>
+);
+
+export const getStaticPaths = async () => {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export default Post;
