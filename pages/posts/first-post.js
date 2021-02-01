58752c830af5b253092c129dfb7a7d81811e4668
Mike <mnightingale@tutanota.com>
2021-02-01 13:51:36 +0000

    Add links

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
-const firstPost = () => <h1>First Post!</h1>;
+import Link from "next/link";
+
+const firstPost = () => (
+  <>
+    <h1>First Post!</h1>
+    <h2>
+      <Link href="/">
+        <a>Return to home</a>
+      </Link>
+    </h2>
+  </>
+);
 
 export default firstPost;
